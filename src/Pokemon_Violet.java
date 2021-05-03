7fc4acac6ca2dce79e9035c28f4ae1434166f384
harrypark960925 <83077836+harrypark960925@users.noreply.github.com>
2021-05-04 00:45:25 +0900

    Update Pokemon_Violet.java

--- src/Pokemon_Violet.java
+++ src/Pokemon_Violet.java
@@ -1,116 +1,243 @@
 import java.util.Random;
-import java.util.Scanner;
-public class Pokemon_Violet {
+import processing.core.PApplet;
+import processing.core.PImage;
+public class Pokemon_Violet extends PApplet{
+	PImage venusaur;  PImage charizard;  PImage blastoise;
+	Pokemon mypokemon = new Pokemon();
+	Pokemon cpupokemon = new Pokemon();
+	Random randomx = new Random();
+	int r1 = randomx.nextInt(3);
+	int r2 = randomx.nextInt(3);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Random randomx = new Random();
-		int r1 = randomx.nextInt(3);
-		int r2 = randomx.nextInt(3);
-		Pokemon mypokemon = new Pokemon();
-		Pokemon cpupokemon = new Pokemon();
+		PApplet.main("Pokemon_Violet");			
+}
+	public void settings() {	
+		size(700,700);
+	}
+	public void setup() {
+		background(255,255,255); 
+		charizard = loadImage("charizard.png");
+		venusaur = loadImage("venusaur.png");
+		blastoise = loadImage("blastoise.jpg");
+		
+		textSize(15); 
+		fill(0,0,0);
 		if (r1==0) {
-			mypokemon= new charizard();
-			System.out.println("Charizard has been chosen for you!");
+			mypokemon= new charizard();			
+			textAlign(LEFT);
+			text("You have chosen Charizard!",20,50);
 		}
 		else if (r1==1) {
-			mypokemon= new venusaur();
-			System.out.println("Venusaur has been chosen for you!");
-
+			mypokemon= new venusaur();			 
+			textAlign(LEFT);
+			text("You have chosen Venusaur!",20,50);
 		}
 		else if (r1==2) {
 			mypokemon= new blastoise();
-			System.out.println("Blastoise has been chosen for you!");
-
+			textAlign(LEFT);
+			text("You have chosen Blastoise!",20,50);
 		}
 		if (r2==0) {
-			cpupokemon= new charizard();
-			System.out.println("Computer has choesn Charizard!");
-
+			cpupokemon= new charizard();			
+			textAlign(RIGHT);
+			text("Computer has chosen Charizard!",680,50);
 		}
 		else if (r2==1) {
-			cpupokemon= new venusaur();
-			System.out.println("Computer has choesn Venusaur!");
-
+			cpupokemon= new venusaur();			
+			textAlign(RIGHT);
+			text("Computer has chosen Venusaur!",680,50);
 		}
 		else if (r2==2) {
 			cpupokemon= new blastoise();
-			System.out.println("Computer has choesn Blastoise!");
+			textAlign(RIGHT);
+			text("Computer has chosen Blastoise!",680,50);
+		}		
+		textAlign(LEFT);
+		text(mypokemon.name+" ("+mypokemon.type+")",60,488);
+		textAlign(RIGHT);
+		text(cpupokemon.name+" ("+cpupokemon.type+")",640,488);		
+		textAlign(LEFT);
+		text("Choose a Move! (1 ~ 4)",250,70);
+		text("1."+ mypokemon.move1.movename,250,90);
+		text("2."+ mypokemon.move2.movename,250,110);
+		text("3."+ mypokemon.move3.movename,250,130);
+		text("4."+ mypokemon.move4.movename,250,150);
+	}	
+	public void draw() {
+		//health bar 
+		fill(255,0,0);
+		rect(60,250,100,10); 
+		rect(530,250,100,10);
+		if(r1==0) {
+			image(charizard,20,270,200,200);
 		}
-		
-		
-		Scanner scan = new Scanner(System.in);
-		while (mypokemon.health > 0) {
-		System.out.println("Choose a Move!");
-		// I know this is maybe not a scanner and a click but not sure how to do that
-		// I think that is someone else's part idk, this is just place holder for structure
-		String move = scan.nextLine();
-		
-		if (move.equals("1")) {
-			System.out.println("Your " + mypokemon.name + " used " + mypokemon.move1.movename);
-			cpupokemon.damage(mypokemon.move1); 		
-			System.out.println();
-
-
+		else if(r1==1) {
+			image(venusaur,20,270,200,200);
 		}
-		if (move.equals("2")) {
-			System.out.println("Your " + mypokemon.name + " used " + mypokemon.move2.movename);
-			cpupokemon.damage(mypokemon.move2);
-			System.out.println();
-
+		else if(r1==2) {
+			image(blastoise,20,270,200,200);
 		}
-		if (move.equals("3")) {
-			System.out.println("Your " + mypokemon.name + "used " + mypokemon.move3.movename);
-			cpupokemon.damage(mypokemon.move3);
-			System.out.println();
-
+		if(r2==0) {
+			image(charizard,500,270,200,200);
 		}
-		if (move.equals("4")) {
-			System.out.println("Your " + mypokemon.name + " used " + mypokemon.move4.movename);
-			cpupokemon.damage(mypokemon.move4);
-			System.out.println();
-
+		else if(r2==1) {
+			image(venusaur,500,270,200,200);
 		}
-		if (cpupokemon.health <=0) {
-			System.out.println("You have won!");
-			break;
-		}
-
-		int cpumove = randomx.nextInt(4)+1;
-		if (cpumove==1) {
-			System.out.println("Enemy " + cpupokemon.name + " used " + cpupokemon.move1.movename);
-			mypokemon.damage(cpupokemon.move1);
-			System.out.println();
-
-		}
-		if (cpumove==2) {
-			System.out.println("Enemy " + cpupokemon.name + " used " + cpupokemon.move2.movename);
-			mypokemon.damage(cpupokemon.move2);
-			System.out.println();
-
-		}
-		if (cpumove==3) {
-			System.out.println("Enemy " + cpupokemon.name + " used " + cpupokemon.move3.movename);
-			mypokemon.damage(cpupokemon.move3);
-			System.out.println();
-
-		}
-		if (cpumove ==4) {
-			System.out.println("Enemy " + cpupokemon.name + " used " + cpupokemon.move4.movename);
-			mypokemon.damage(cpupokemon.move4);
-			System.out.println();
-
-		}
-		
-	
-		if (mypokemon.health <=0) {
-			System.out.println("You have lost!");
-			break;
-		}
-		
-		System.out.println(mypokemon.name+ "'s health:"+ mypokemon.health);
-		System.out.println(cpupokemon.name+"'s health:"+ cpupokemon.health);
-	
+		else if(r2==2) {
+			image(blastoise,500,270,200,200);
+		}		
 	}
+	public void keyPressed() {
+		Random randomx = new Random();
+		textSize(15); 
+		fill(0,0,0);
+		textAlign(LEFT);
+		
+		int cpumove = randomx.nextInt(4)+1;
+			if(keyCode == '1') {
+				background(255,255,255);
+				text("Your " + mypokemon.name + " used " + mypokemon.move1.movename+"!",40,190);
+				text(mypokemon.name+" ("+mypokemon.type+")",60,488);
+				cpupokemon.damage(mypokemon.move1); 
+				if (cpumove==1) {		
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move1.movename,400,190);
+					mypokemon.damage(cpupokemon.move1);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==2) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move2.movename,400,190);
+					mypokemon.damage(cpupokemon.move2);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==3) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move3.movename,400,190);
+					mypokemon.damage(cpupokemon.move3);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==4) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move4.movename,400,190);
+					mypokemon.damage(cpupokemon.move4);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+			}
+			else if (keyCode == '2') {
+				background(255,255,255);
+				text("Your " + mypokemon.name + " used " + mypokemon.move2.movename +"!",40,190);
+				text(mypokemon.name+" ("+mypokemon.type+")",60,488);
+				cpupokemon.damage(mypokemon.move2); 
 
-}}
+				if (cpumove==1) {		
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move1.movename,400,190);
+					mypokemon.damage(cpupokemon.move1);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==2) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move2.movename,400,190);
+					mypokemon.damage(cpupokemon.move2);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==3) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move3.movename,400,190);
+					mypokemon.damage(cpupokemon.move3);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==4) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move4.movename,400,190);
+					mypokemon.damage(cpupokemon.move4);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+			}
+			else if (keyCode == '3') {
+				background(255,255,255);
+				text("Your " + mypokemon.name + " used " + mypokemon.move3.movename+"!",40,190);
+				text(mypokemon.name+" ("+mypokemon.type+")",60,488);
+				cpupokemon.damage(mypokemon.move3); 
 
+				if (cpumove==1) {		
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move1.movename,400,190);
+					mypokemon.damage(cpupokemon.move1);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==2) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move2.movename,400,190);
+					mypokemon.damage(cpupokemon.move2);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==3) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move3.movename,400,190);
+					mypokemon.damage(cpupokemon.move3);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==4) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move4.movename,400,190);
+					mypokemon.damage(cpupokemon.move4);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+			}
+			else if (keyCode == '4') {
+				background(255,255,255);
+				text("Your " + mypokemon.name + " used " + mypokemon.move4.movename+"!",40,190);
+				text(mypokemon.name+" ("+mypokemon.type+")",60,488);
+				cpupokemon.damage(mypokemon.move4); 
+
+				if (cpumove==1) {		
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move1.movename,400,190);
+					mypokemon.damage(cpupokemon.move1);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==2) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move2.movename,400,190);
+					mypokemon.damage(cpupokemon.move2);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==3) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move3.movename,400,190);
+					mypokemon.damage(cpupokemon.move3);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+				else if (cpumove==4) {
+					text("Enemy " + cpupokemon.name + " used " + cpupokemon.move4.movename,400,190);
+					mypokemon.damage(cpupokemon.move4);
+					text(cpupokemon.name+" ("+cpupokemon.type+")",500,488);		
+				}
+			}
+			
+			text("Choose a Move! (1 ~ 4)",250,70);
+			text("1."+ mypokemon.move1.movename,250,90);
+			text("2."+ mypokemon.move2.movename,250,110);
+			text("3."+ mypokemon.move3.movename,250,130);
+			text("4."+ mypokemon.move4.movename,250,150);
+			
+			text("Your "+mypokemon.name+ "'s health: "+ mypokemon.health,30,510);
+			text("Enemy's"+cpupokemon.name+"'s health:"+ cpupokemon.health,460,510);
+
+		
+			if (cpupokemon.health <=0 && mypokemon.health > 0) {
+				background(255,255,255);
+				textSize(25); 
+				fill(0,0,0);
+				textAlign(CENTER);
+				text("You have WON!!",350,550);
+			}
+
+			else if (mypokemon.health <=0 && cpupokemon.health > 0) {
+				background(255,255,255);
+				textSize(50); 
+				fill(0,0,0);
+				textAlign(CENTER);
+				text("You have LOST!!",350,550);			
+			}
+			else if (mypokemon.health<=0 && cpupokemon.health <=0 ) {
+				background(255,255,255);
+				textSize(50); 
+				fill(0,0,0);
+				textAlign(CENTER);
+				text("TIE",350,550);
+			}
+	}
+	
+
+	
+}
+	
+	
